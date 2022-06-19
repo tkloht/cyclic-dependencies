2b25a4587497702478bd9b28ea4c90afed3651c7
Tobias Kloht <mail@kloht.com>
2022-06-19 12:27:54 +0200

    add argument to exit with error if cycle found

--- bin/workspace-cycles.js
+++ bin/workspace-cycles.js
@@ -3,6 +3,7 @@
 import findWorkspaceCycles from "../src/index.js"
 
 async function run() {
+  const args = process.argv.slice(2)
   try {
     const cycles = await findWorkspaceCycles()
 
@@ -11,6 +12,9 @@ async function run() {
         "Cyclic dependencies found in workspace: ",
         cycles.map((packages) => packages.join(" -> "))
       )
+      if (args.includes("--reject")) {
+        process.exit(1)
+      }
     } else {
       console.log("No cyclic dependencies found in workspace")
     }
