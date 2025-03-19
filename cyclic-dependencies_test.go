4ac93e10fb45235ce851544f00228525a7f7c538
Tobias Kloht <mail@kloht.com>
2025-03-20 00:38:25 +0100

    use table test

--- cyclic-dependencies_test.go
+++ cyclic-dependencies_test.go
@@ -21,15 +21,39 @@ func initFixture(name string) {
 		fmt.Println("Error: ", error)
 
 	}
-
 }
 
+type testCase struct {
+	fixtureName string
+	expectedMatches []string
+}
+
+var testCases = []testCase{
+	{"default", []string{"example1/package.json", "example2/package.json"}},
+	{"star", []string{"packages/example1/package.json", "packages/example2/package.json"}},
+	// {"pnpm-doublestar", []string{"packages/backend/one/package.json", "packages/backend/two/package.json", "packages/frontend/one/package.json", "packages/frontend/two/package.json"}},
+}
+
+
+
 func TestCyclicDependencies(t *testing.T) {
-	initFixture("default")
-	result := findWorkspacePackages()
-	expected := []string{"example1/package.json", "example2/package.json"}
-	if !slices.Equal(result, expected) {
-		t.Errorf("Expected %v, got %v",expected, result)
+
+	for _, tc := range testCases {
+		initFixture(tc.fixtureName)
+		result := findWorkspacePackages()
+		os.Chdir("../..")// reset to the root directory
+		slices.Sort(result)
+		slices.Sort(tc.expectedMatches)
+		if !slices.Equal(result, tc.expectedMatches) {
+			t.Errorf("Expected %v, got %v",tc.expectedMatches, result)
+		}
 	}
+
+	// initFixture("default")
+	// result := findWorkspacePackages()
+	// expected := []string{"example1/package.json", "example2/package.json"}
+	// if !slices.Equal(result, expected) {
+	// 	t.Errorf("Expected %v, got %v",expected, result)
+	// }
 }
 
